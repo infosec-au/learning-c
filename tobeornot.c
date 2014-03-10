e70b52df1ea55e47536079e981d55da93ee7cfcd
Shubham Shah <admin@shubh.am>
2014-03-10 19:52:09 +1100

    Lotsa exercises

---
+++ tobeornot.c
@@ -0,0 +1,12 @@
+// Shubham Shah
+// 9th March 2014
+// This program prints out "To be, or not to be" into the console/terminal
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	printf("To be, or not to be?");
+
+	return EXIT_SUCCESS;
+}
