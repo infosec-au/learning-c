fb48ea81971aecb4a3ae64987efac413252708ef
Shubham Shah <shahshubham369@gmail.com>
2014-04-11 22:03:57 +1000

    Task2B

--- Week6/Task2B/bmpServer.c
+++ Week6/Task2B/bmpServer.c
@@ -17,7 +17,8 @@
 
 int waitForConnection (int serverSocket);
 int makeServerSocket (int portno);
-void serveBMP (int socket);
+static void serveHTML (int socket);
+void serveBMP (int socket, double posX, double posY, int zoomCount);
 
 #define SIMPLE_SERVER_VERSION 1.0
 #define REQUEST_BUFFER_SIZE 1000
@@ -36,34 +37,58 @@ int main (int argc, char *argv[]) {
    printf ("************************************\n");
    
    char request[REQUEST_BUFFER_SIZE];
-   
+   // Declaring variables to be set as user input from GET request
+   char requestCheckX;
+   char requestCheckY;
+   char requestCheckZ;
+   double requestX;
+   double requestY;
+   int requestZ;
+
    int numberServed = 0;
    while (numberServed < NUMBER_OF_PAGES_TO_SERVE) {
-      
-      printf ("*** So far served %d pages ***\n", numberServed);
-      
-      int connectionSocket = waitForConnection (serverSocket);
-      // wait for a request to be sent from a web browser, open a new
-      // connection for this conversation
-      
-      // read the first line of the request sent by the browser  
-      int bytesRead;
-      bytesRead = read (connectionSocket, request, (sizeof request)-1);
-      assert (bytesRead >= 0); 
-      // were we able to read any data from the connection?
-      printf("first line: %d\n", bytesRead);
-      // print entire request to the console 
-      printf (" *** Received http request ***\n %s\n", request);
-      
-      //send the browser a simple html page using http
-      printf (" *** Sending http response ***\n");
-      printf(" *** Connection Socket *** : %d\n", connectionSocket);
-      serveBMP(connectionSocket);
-      
-      // close the connection after sending the page- keep aust beautiful
-      close(connectionSocket);
-      
-      numberServed++;
+
+    printf ("*** So far served %d pages ***\n", numberServed);
+
+    int connectionSocket = waitForConnection (serverSocket);
+    // wait for a request to be sent from a web browser, open a new
+    // connection for this conversation
+
+    // read the first line of the request sent by the browser  
+    int bytesRead;
+    bytesRead = read (connectionSocket, request, (sizeof request)-1);
+    assert (bytesRead >= 0);
+    // were we able to read any data from the connection?
+    printf(" *** Count of first line of HTTP Request *** : %d\n", bytesRead);
+    // print entire request to the console 
+    printf (" *** Received http request ***\n %s\n", request);
+
+    //send the browser a simple html page using http
+    printf (" *** Sending http response ***\n");
+    printf(" *** Connection Socket *** : %d\n", connectionSocket);
+
+    // Reading in user input (string scanf)
+    // if the first %c is empty, serve bmp
+    // else, if the first %c is a X, serve html
+    sscanf(request, "GET /%c%lf_%c%lf_%c%d.bmp", &requestCheckX, &requestX, &requestCheckY, &requestY, &requestCheckZ, &requestZ);
+
+    // Makes sure that both capitalised and uncapitalised input is accepted
+    if ((requestCheckX != 'X') || (requestCheckX != 'x')) {
+
+        serveHTML(connectionSocket);
+
+    } else if ((requestCheckX == 'X' || requestCheckX == 'x')
+                && (requestCheckY == 'Y' || requestCheckY == 'y')
+                && (requestCheckZ == 'Z' || requestCheckZ == 'z')) {
+
+        serveBMP(connectionSocket, requestX, requestY, requestZ);
+
+    }
+
+    // close the connection after sending the page- keep aust beautiful
+    close(connectionSocket);
+
+    numberServed++;
    } 
    
    // close the server connection after we are done- keep aust beautiful
@@ -73,22 +98,40 @@ int main (int argc, char *argv[]) {
    return EXIT_SUCCESS; 
 }
 
-void serveBMP (int socket) {
-   char* message;
-   
-   // first send the http response header
-   
-   // (if you write stings one after another like this on separate
-   // lines the c compiler kindly joins them togther for you into
-   // one long string)
-   message = "HTTP/1.0 200 OK\r\n"
+static void serveHTML (int socket) {
+    char* message;
+
+    // first send the http response header
+    message =
+        "HTTP/1.0 200 Found\n"
+        "Content-Type: text/html\n"
+        "\n";
+    printf ("about to send=> %s\n", message);
+    write (socket, message, strlen (message));
+
+    message =
+        "<!DOCTYPE html>\n"
+        "<script src=\"https://almondbread.cse.unsw.edu.au/tiles.js\"></script>"
+          "\n";
+    write (socket, message, strlen (message));
+}
+
+void serveBMP (int socket, double posX, double posY, int zoomCount) {
+    char* message;
+
+    // first send the http response header
+
+    // (if you write stings one after another like this on separate
+    // lines the c compiler kindly joins them togther for you into
+    // one long string)
+    message = "HTTP/1.0 200 OK\r\n"
                 "Content-Type: image/bmp\r\n"
                 "\r\n";
-   printf ("about to send=> %s\n", message);
-   write (socket, message, strlen (message));
-   
-   // now send the BMP
-   unsigned char bmp[] = {
+    printf ("about to send=> %s\n", message);
+    write (socket, message, strlen (message));
+
+    // now send the BMP
+    unsigned char bmp[] = {
      0x42,0x4d,0x5a,0x00,0x00,0x00,0x00,0x00,
      0x00,0x00,0x36,0x00,0x00,0x00,0x28,0x00,
      0x00,0x00,0x03,0x00,0x00,0x00,0x03,0x00,
@@ -103,69 +146,69 @@ void serveBMP (int socket) {
      0x00,0x0d};
 
 
-   write (socket, bmp, sizeof(bmp));
+    write (socket, bmp, sizeof(bmp));
 }
 
 
 // start the server listening on the specified port number
 int makeServerSocket (int portNumber) { 
-   
-   // create socket
-   int serverSocket = socket (AF_INET, SOCK_STREAM, 0);
-   assert (serverSocket >= 0);   
-   // error opening socket
-   
-   // bind socket to listening port
-   struct sockaddr_in serverAddress;
-   bzero ((char *) &serverAddress, sizeof (serverAddress));
-   
-   serverAddress.sin_family      = AF_INET;
-   serverAddress.sin_addr.s_addr = INADDR_ANY;
-   serverAddress.sin_port        = htons (portNumber);
-   
-   // let the server start immediately after a previous shutdown
-   int optionValue = 1;
-   setsockopt (
+
+    // create socket
+    int serverSocket = socket (AF_INET, SOCK_STREAM, 0);
+    assert (serverSocket >= 0);   
+    // error opening socket
+
+    // bind socket to listening port
+    struct sockaddr_in serverAddress;
+    bzero ((char *) &serverAddress, sizeof (serverAddress));
+
+    serverAddress.sin_family      = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port        = htons (portNumber);
+
+    // let the server start immediately after a previous shutdown
+    int optionValue = 1;
+    setsockopt (
       serverSocket,
       SOL_SOCKET,
       SO_REUSEADDR,
       &optionValue, 
       sizeof(int)
-   );
+    );
 
-   int bindSuccess = 
+    int bindSuccess = 
       bind (
          serverSocket, 
          (struct sockaddr *) &serverAddress,
          sizeof (serverAddress)
       );
-   
-   assert (bindSuccess >= 0);
-   // if this assert fails wait a short while to let the operating 
-   // system clear the port before trying again
-   
-   return serverSocket;
+
+    assert (bindSuccess >= 0);
+    // if this assert fails wait a short while to let the operating 
+    // system clear the port before trying again
+
+    return serverSocket;
 }
 
 // wait for a browser to request a connection,
 // returns the socket on which the conversation will take place
 int waitForConnection (int serverSocket) {
-   // listen for a connection
-   const int serverMaxBacklog = 10;
-   listen (serverSocket, serverMaxBacklog);
-   
-   // accept the connection
-   struct sockaddr_in clientAddress;
-   socklen_t clientLen = sizeof (clientAddress);
-   int connectionSocket = 
+    // listen for a connection
+    const int serverMaxBacklog = 10;
+    listen (serverSocket, serverMaxBacklog);
+
+    // accept the connection
+    struct sockaddr_in clientAddress;
+    socklen_t clientLen = sizeof (clientAddress);
+    int connectionSocket = 
       accept (
          serverSocket, 
          (struct sockaddr *) &clientAddress, 
          &clientLen
       );
-   
-   assert (connectionSocket >= 0);
-   // error on accept
-   
-   return (connectionSocket);
+
+    assert (connectionSocket >= 0);
+    // error on accept
+
+    return (connectionSocket);
 }
